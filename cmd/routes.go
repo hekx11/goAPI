673811a91a408bb2a1191c3a39628f2dd9157324
Tomek Popek <t.popek123@gmail.com>
2022-10-19 13:29:34 +0200

    last update

---
+++ cmd/routes.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/hekx11/goapi/handlers"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/", handlers.ListFacts)
+	app.Post("/fact", handlers.CreateFact)
+}
--- models/models.go
+++ models/models.go
@@ -5,5 +5,5 @@ import "gorm.io/gorm"
 type Fact struct {
 	gorm.Model
 	Question string `json:"question" gorm:"text;not null;default:null`
-	Answer   string `json:"question" gorm:"text;not null;default:null`
+	Answer   string `json:"answer" gorm:"text;not null;default:null`
 }
